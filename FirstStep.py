2130d7e7ca713e5ed52970473b13e992199d92d7
Tran Bao Huu <tranbaohuu@gmail.com>
2018-10-16 23:02:39 +0700

    Initial commit

---
+++ FirstStep.py
@@ -0,0 +1,15 @@
+print('Hello Huu Tran')
+
+abc = 123
+
+print(abc)
+
+mystring = 'tranbaohuu'
+
+print(mystring[-1])
+print(mystring[9])
+
+# lấy từ index 2 trở lên
+print(mystring[2:])
+# lấy từ index 0 đến index 2
+print(mystring[:2])
