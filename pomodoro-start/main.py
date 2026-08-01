6a536b9f395322e1382d88867eea81186d42d7f5
Trần Bảo Hữu <tranbaohuu@gmail.com>
2026-08-01 23:50:36 +0700

    refactor main.py to improve UI layout and add buttons for start and reset

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Canvas, PhotoImage
+import tkinter
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -20,12 +21,21 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.configure(padx=100, pady=50)
+window.configure(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(window, width=200, height=224)
-photo =  PhotoImage(file="tomato.png")
+# title
+tkinter.Label(window, text="Timer", font=(FONT_NAME, 40), bg=YELLOW, fg=GREEN).grid(row=0, column=1)
+
+# Pomodoro Timer Text
+canvas = Canvas(window, width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=photo)
-canvas.create_text(103,130,text="00:00", font=(FONT_NAME,35,"bold"), fill="white")
-canvas.pack()
+canvas.create_text(103, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.grid(row=1, column=1)
+
+# Buttons
+Button(window, text="Start", highlightthickness=0).grid(row=2, column=0)
+Button(window, text="Reset", highlightthickness=0).grid(row=2, column=2)
+# canvas.pack()
 
 window.mainloop()
